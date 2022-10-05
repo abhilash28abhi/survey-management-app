ff92918e656b330a1de83af0303d1f7cd631307a
abhilash28abhi <abhilash28.abhi@gmail.com>
2022-10-05 10:13:00 +0530

    initial changes for the proj setup

---
+++ src/main/java/com/surveymanagement/app/SurveyManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.surveymanagement.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surveymanagement/app/SurveyManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surveymanagement.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

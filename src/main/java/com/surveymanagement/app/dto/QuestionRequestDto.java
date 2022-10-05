082ae5cbb2a11e2074ef66f5a7b62466227b6db2
abhilash28abhi <abhilash28.abhi@gmail.com>
2022-10-05 19:38:21 +0530

    corrected for boolean property

--- src/main/java/com/surveymanagement/app/dto/QuestionRequestDto.java
+++ src/main/java/com/surveymanagement/app/dto/QuestionRequestDto.java
@@ -3,13 +3,11 @@ package com.surveymanagement.app.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 @Data
 public class QuestionRequestDto {
 
     @NotBlank(message = "Description of the question is required.")
     private String description;
-    @NotEmpty(message = "Active state value is required.")
     private boolean active;
 }

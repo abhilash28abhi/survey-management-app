789e1a82a16f34c7bf0dbd37028c8f23f6799715
abhilash28abhi <abhilash28.abhi@gmail.com>
2022-10-05 17:11:16 +0530

    corrected repository return data

--- src/main/java/com/surveymanagement/app/repository/OptionRepository.java
+++ src/main/java/com/surveymanagement/app/repository/OptionRepository.java
@@ -1,7 +1,6 @@
 package com.surveymanagement.app.repository;
 
 import com.surveymanagement.app.model.Option;
-import com.surveymanagement.app.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +9,5 @@ import java.util.List;
 @Repository
 public interface OptionRepository extends JpaRepository<Option, Long> {
 
-    List<Question> findByQuestionId(Long questionId);
+    List<Option> findByQuestionId(Long questionId);
 }
--- src/main/java/com/surveymanagement/app/repository/QuestionRepository.java
+++ src/main/java/com/surveymanagement/app/repository/QuestionRepository.java
@@ -1,7 +1,6 @@
 package com.surveymanagement.app.repository;
 
 import com.surveymanagement.app.model.Question;
-import com.surveymanagement.app.model.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +9,5 @@ import java.util.List;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    List<Survey> findBySurveyId(Long surveyId);
+    List<Question> findBySurveyId(Long surveyId);
 }
